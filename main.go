2ed9f89fe8166926e99035450359678b8d1c46ce
linhdmn <mnhatlinh.doan@gmail.com>
2025-10-04 00:50:35 +0700

    test: verify pre-commit hook is working

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 // Version information (set by GoReleaser).
 var (
-	version = "v0.1.4"
+	version = "v0.1.4" // test comment
 	commit  = "none"
 	date    = "unknown"
 	builtBy = "local"
